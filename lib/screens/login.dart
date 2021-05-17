56fba1601245b853b09a2512333caeec26c60cf0
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-05-17 00:14:34 -0300

    Add a Card to Form
    Former-commit-id: b26fdf81e9b784e03f58eb37bee929e16df4e469

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -27,59 +27,69 @@ class LoginPageState extends State<LoginPage> {
     // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Nome Completo'),
-            keyboardType: TextInputType.text,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Porfavor insira o nome completo';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Contato'),
-            keyboardType: inputType,
-            // Validating the text input
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Porfavor insira o numero ou endereço de email válido.';
-              } else if (onlyEmail.hasMatch(value)) {
-                inputType = TextInputType.emailAddress;
-              } else if (onlyNumber.hasMatch(value)) {
-                inputType = TextInputType.phone;
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 35.0, horizontal: 35.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text('Iniciando a Pesquisa')));
-                }
-              },
-              child: Text(
-                'Iniciar Pesquisa',
-                textScaleFactor: 2,
-                style: TextStyle(color: Colors.black),
+      child: 
+        Card(  
+          child: Column(  
+            mainAxisSize: MainAxisSize.min,  
+            children: <Widget>[  
+              Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Nome Completo'),
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Porfavor insira o nome completo';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: 'Contato'),
+                    keyboardType: inputType,
+                    // Validating the text input
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Porfavor insira o numero ou endereço de email válido.';
+                      } else if (onlyEmail.hasMatch(value)) {
+                        inputType = TextInputType.emailAddress;
+                      } else if (onlyNumber.hasMatch(value)) {
+                        inputType = TextInputType.phone;
+                      } else if (!onlyEmail.hasMatch(value) || !onlyNumber.hasMatch(value)){
+                        return 'Porfavor insira o numero ou endereço de email válido.';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 35.0, horizontal: 35.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(SnackBar(content: Text('Iniciando a Pesquisa')));
+                        }
+                      },
+                      child: Text(
+                        'Iniciar Pesquisa',
+                        textScaleFactor: 2,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(fontSize: 12.0),
+                        primary: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 25.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.blue[600])
+                        )
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.blue[600])
-                  )
-                )
-              ),
-            ),
-          ),
-        ],
-      ),
+        ])
+      )// Login card.
     );
   }
 }
