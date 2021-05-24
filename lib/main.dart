9b536c4f8b8eb1d41df197b57fbbfdbf1a815375
jimmy starling <jimmystarlingdev@gmail.com>
2021-05-24 17:08:59 -0300

    Renaming root Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import './screens/login.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Root());
 
-class MyApp extends StatelessWidget {
+class Root extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
