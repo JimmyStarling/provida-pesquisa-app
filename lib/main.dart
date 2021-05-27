d38bd0463af5bf8b4e6f481a1fd32dd1f6afbf4e
jimmy starling <jimmystarlingdev@gmail.com>
2021-05-27 17:04:28 -0300

    Change Root widget & Add Main route widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app_pesquisa_de_satisfacao/screens/questions.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import './screens/login.dart';
@@ -13,7 +15,35 @@ class Root extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // ignore: missing_return
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/':
+            return CupertinoPageRoute(
+              builder: (_) => LoginPage(), settings: settings);
+          case '/question/1':
+            return CupertinoPageRoute(
+              builder: (_) => Question1(), settings: settings);
+          case '/question/2':
+            return CupertinoPageRoute(
+              builder: (_) => Question2(), settings: settings);
+          case '/question/3':
+            return CupertinoPageRoute(
+              builder: (_) => Question3(), settings: settings);
+          case '/question/4':
+            return CupertinoPageRoute(
+              builder: (_) => Question4(), settings: settings);
+        }
+      },
     );
   }
 }
