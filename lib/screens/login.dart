16c083d55a07f1de3fb2450aca8afbb76405e872
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-05-31 00:05:20 -0300

    Insert database methods and data.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,6 +4,8 @@
  * 
  */
 
+import 'package:app_pesquisa_de_satisfacao/models/client_model.dart';
+import 'package:app_pesquisa_de_satisfacao/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:app_pesquisa_de_satisfacao/screens/nurse_questions.dart';
 import '../constants.dart';
@@ -21,6 +23,21 @@ class LoginPageState extends State<LoginPage> {
   var inputType = TextInputType.text;
   var onlyNumber = RegExp(r'^[0-9]+$');
   var onlyEmail = RegExp(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
+
+  // Local variables to database
+  Map<String, String> _client = {
+    'name':'name',
+    'surname':'surname'
+  };
+	List<Client> _clients = [];
+  List<Client> _fakeClients = [];
+
+  // Visual list of clients registered
+  // What occurs if there isn't data
+	List<Widget> get _clientList => 
+    (_clients != null)? 
+      _clients.map((client) => format(client)).toList(): _fakeClients.map((client) => format(client)).toList();
+	TextStyle _style = TextStyle(color: Colors.white, fontSize: 24);
 
   bool isEmailOrNumber;
 
@@ -45,8 +62,14 @@ class LoginPageState extends State<LoginPage> {
                     decoration: const InputDecoration(labelText: 'Nome Completo'),
                     keyboardType: TextInputType.text,
                     validator: (value) {
+                      // Split value in two
+                      var fullName = value.split(" ");
+
                       if (value == null || value.isEmpty) {
                         return 'Porfavor insira o nome completo';
+                      } else {
+                        _client['name'] = fullName[0].toString();
+                        _client['surname'] = fullName[1].toString();
                       }
                       return null;
                     },
@@ -56,6 +79,9 @@ class LoginPageState extends State<LoginPage> {
                     keyboardType: inputType,
                     // Validating the text input
                     validator: (value) {
+                      // Get readable string
+                      var contactAdress = value.toString();
+
                       if (value == null || value.isEmpty) {
                         return 'Porfavor insira o numero ou endereço de email válido.';
                       } else if (onlyEmail.hasMatch(value)) {
@@ -78,6 +104,8 @@ class LoginPageState extends State<LoginPage> {
                     child: TextButton(
                       onPressed: () {
                         if (_formKey.currentState.validate()) {
+                          // Saving all local informations to the database
+                          _save();
                           Navigator.pushNamed(context, '/nurse/question1');
                           ScaffoldMessenger.of(context)
                               .showSnackBar(SnackBar(content: Text('Iniciando a Pesquisa')));
@@ -130,6 +158,18 @@ class LoginPageState extends State<LoginPage> {
                       ),
                     ),
                   ),
+                  TextButton(
+                    onPressed: (){
+                      _client['name'] = 'testing';
+                      _client['surname'] = 'testing2';
+                      _save();
+                    },
+                    child: Text(
+                      'Insert Fake Data',
+                      style: TextStyle(color: Colors.indigo[900]),
+                    ),
+                  ),
+                  ListView( children: _clientList )
                 ],
               ),// Column Child
           ])// Column Form
@@ -137,4 +177,72 @@ class LoginPageState extends State<LoginPage> {
       )// Padding
     );// Scaffold
   }
+  Widget format(Client client) {
+    return Dismissible(
+          key: Key(client.id.toString()),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(12, 6, 12, 4),
+            child: TextButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(client.name, style: _style),
+                  Text(client.surname, style: _style),
+                  Icon(client.complete == true ? Icons.radio_button_checked : Icons.radio_button_unchecked, color: Colors.white)
+                ]
+              ),
+              onPressed: () => _completed(client),
+            )
+          ),
+          onDismissed: (DismissDirection direction) => _delete(client),
+        );
+  }
+
+  void _completed(Client client) async {
+
+		client.complete = !client.complete;
+		dynamic result = await DB.update(Client.table, client);
+		print(result);
+		refresh();
+	}
+
+
+  void _delete(Client client) async {
+		
+		DB.delete(Client.table, client);
+		refresh();
+	}
+
+	void _save() async {
+
+		Navigator.of(context).pop();
+		Client client = Client(
+			name: _client[0],
+      surname: _client[1],
+			complete: false
+		);
+
+    // Inserting and refreshing the cache
+		await DB.insert(Client.table, client);
+		setState(() => _client = {} );
+		refresh();
+	}
+
+	@override
+	void initState() {
+
+    _client['name'] = 'Jimmy';
+    _client['surname'] = 'Starling';
+    _save();
+
+		refresh();
+		super.initState();
+	}
+
+	void refresh() async {
+
+		List<Map<String, dynamic>> _results = await DB.query(Client.table);
+		_clients = _results.map((client) => Client.fromMap(client)).toList();
+		setState(() { });
+	}
 }
