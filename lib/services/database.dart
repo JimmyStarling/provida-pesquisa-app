2f17bb4313d7d42f5371c0c050ace621e7bbcd2f
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-06-08 16:59:21 -0300

    Modify database methods.
    Former-commit-id: 01f4032fbd07bb2b3c95505813a22e42cd190b6b [formerly 0163046181714add65b4db57517a6c11153f8022]
    Former-commit-id: 7f1fe2cab4836258ce618d9cce9b6a1b46f3d1b5

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,42 +1,108 @@
 import 'dart:async';
-import 'package:app_pesquisa_de_satisfacao/models/model.dart';
+import 'dart:developer';
+import 'package:app_pesquisa_de_satisfacao/models/client.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-
-abstract class DB {
-
-	static Database _db;
+abstract class DatabaseHelper {
 
 	static int get _version => 1;
-  // Init the database with path and version
-	static Future<void> init() async {
 
-		if (_db != null) { return; }
+  static Database _database;
+  static final _databaseName = "banco_pesquisa.db";
 
-		try {
-			String _path = await getDatabasesPath() + 'pesquisa_db';
-			_db = await openDatabase(_path, version: _version, onCreate: onCreate);
-		}
-		catch(ex) { 
-			print(ex);
-		}
-	}
+  static final table = 'clients';
 
-	static void onCreate(Database db, int version) async =>
-		await db.execute('CREATE TABLE client ('
-                      'id INTEGER PRIMARY KEY NOT NULL,'
-                      'name STRING, ' 
-                      'surname STRING, ' 
-                      'complete BOOLEAN)');
+  static final columnId = '_id';
+  static final columnName = 'name';
+  static final columnComplete = 'complete';
 
-  // Query data
-	static Future<List<Map<String, dynamic>>> query(String table) async => _db.query(table);
-  // Insert data
-	static Future<int> insert(String table, Model model) async =>
-		await _db.insert(table, model.toMap());
-	// Update data
-	static Future<int> update(String table, Model model) async =>
-		await _db.update(table, model.toMap(), where: 'id = ?', whereArgs: [model.id]);
-  // Delete data
-	static Future<int> delete(String table, Model model) async =>
-		await _db.delete(table, where: 'id = ?', whereArgs: [model.id]);
+  Future<Database> get database async {
+    if (_database != null)
+    return _database;
+
+    // if _database is null we instantiate it
+    _database = await initDatabase();
+    return _database;
+  }
+
+  initDatabase () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // Open database
+    _database = await openDatabase(
+      join(await getDatabasesPath(), _databaseName),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE client ('
+                          '$columnId INTEGER PRIMARY KEY NOT NULL,'
+                          '$columnName STRING, ' 
+                          '$columnComplete BOOLEAN)',
+        );
+      },
+      version: _version,
+    );
+    
+    var kafka = Clients(
+      id: 0,
+      name: 'Kafka',
+      complete: true,
+    );
+    await insertClient(kafka);
+    print(await getClients());
+  }
+  // Define a function that inserts clients into the database
+  Future<void> insertClient(Clients client) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    await db.insert(
+      'clients',
+      client.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // A method that retrieves all the clients from the clients table.
+  Future<List<Clients>> getClients() async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('clients');
+
+    // Convert the List<Map<String, dynamic> into a List<Client>.
+    return List.generate(maps.length, (i) {
+      return Clients(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        complete: maps[i]['complete'],
+      );
+    });
+  }
+
+  Future<void> updateClient(Clients client) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Update the given Dog.
+    await db.update(
+      'clients',
+      client.toMap(),
+      // Ensure that the Clients has a matching id.
+      where: 'id = ?',
+      // Pass the Client's id as a whereArg to prevent SQL injection.
+      whereArgs: [client.id],
+    );
+  }
+
+  Future<void> deleteClient(int id) async {
+    final db = await database;
+
+    await db.delete(
+      'clients',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
 }
