d6905244555f9c4fec8f153e2c066b10cc17e3c2
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-06-24 22:58:59 -0300

    Change from sqflite to idb_sqflite.
    Former-commit-id: 27c197c10db5b56804d89c2592d57d9430b743a2

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,14 +2,20 @@ import 'dart:async';
 import 'dart:developer';
 import 'package:app_pesquisa_de_satisfacao/models/client.dart';
 import 'package:flutter/widgets.dart';
+import 'package:idb_sqflite/idb_sqflite.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:idb_sqflite/idb_sqflite.dart' as mDatabase; 
 abstract class DatabaseHelper {
 
 	static int get _version => 1;
 
-  static Database _database;
+  static mDatabase.Database _database;
   static final _databaseName = "banco_pesquisa.db";
+
+
+  static var store;
+  static var key;
 
   static final table = 'clients';
 
@@ -18,7 +24,7 @@ abstract class DatabaseHelper {
   static final columnQuestions = 'questions';
   static final columnComplete = 'complete';
 
-  Future<Database> get database async {
+  Future<mDatabase.Database> get database async {
     if (_database != null)
     return _database;
 
@@ -28,10 +34,22 @@ abstract class DatabaseHelper {
   }
 
   initDatabase () async {
+    var factory = getIdbFactorySqflite(databaseFactory);
+
     log('D/ Init the database.');
     WidgetsFlutterBinding.ensureInitialized();
     // Open database
-    _database = await openDatabase(
+    _database = await factory.open(
+      _databaseName, 
+      version: _version,
+      onUpgradeNeeded: (VersionChangeEvent event){
+        mDatabase.Database databaseEvent = event.database;
+        databaseEvent.createObjectStore(table, autoIncrement: true);
+      }
+    );
+    
+    /**
+    await openDatabase(
       join(await getDatabasesPath(), _databaseName),
       onCreate: (db, version) {
         return db.execute(
@@ -44,6 +62,7 @@ abstract class DatabaseHelper {
       },
       version: _version,
     );
+    **/
     
     var kafka = Clients(
       id: 0,
@@ -57,12 +76,17 @@ abstract class DatabaseHelper {
   Future<void> insertClient(Clients client) async {
     // Get a reference to the database.
     final db = await database;
+    // Transaction variable
+    final mTransaction = db.transaction(table, idbModeReadWrite);
+    // Update client table via transaction
+    key = await store.put(client.toMap());
+    await mTransaction.completed; 
 
-    await db.insert(
+    /*await db.insert(
       'clients',
       client.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
-    );
+    );*/
   }
 
   // A method that retrieves all the clients from the clients table.
@@ -70,9 +94,16 @@ abstract class DatabaseHelper {
     // Get a reference to the database.
     final db = await database;
 
-    // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await db.query('clients');
+    // Getting transactioned client as List
+    final clients =  db.transaction(table, idbModeReadOnly) as List<Map<String, dynamic>>;
+    // It returns a object and convert it to maps
+    final List<Map<String, dynamic>> maps = clients;// Here was .query()
 
+    log('The transaction value is ${db.transaction(table, idbModeReadOnly)}');
+    log('The clients value is ${clients.toString()}');
+
+    var value = await store.getObject(key);
+    
     // Convert the List<Map<String, dynamic> into a List<Client>.
     return List.generate(maps.length, (i) {
       return Clients(
@@ -84,11 +115,17 @@ abstract class DatabaseHelper {
     });
   }
 
+  /**
   Future<void> updateClient(Clients client) async {
     // Get a reference to the database.
     final db = await database;
+    // Transaction variable
+    final mTransaction = db.transaction(table, idbModeReadWrite);
+    // Update client table via transaction
+    var store = mTransaction.objectStore(table);
+    var key = await store.put(client.toMap());
+    await mTransaction.completed; 
 
-    // Update the given Dog.
     await db.update(
       'clients',
       client.toMap(),
@@ -97,8 +134,11 @@ abstract class DatabaseHelper {
       // Pass the Client's id as a whereArg to prevent SQL injection.
       whereArgs: [client.id],
     );
+    
   }
+  **/
 
+  /**
   Future<void> deleteClient(int id) async {
     final db = await database;
 
@@ -108,5 +148,6 @@ abstract class DatabaseHelper {
       whereArgs: [id],
     );
   }
+  **/
 
 }
