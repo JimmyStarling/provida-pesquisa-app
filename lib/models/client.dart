f008335e2979622cc17b49a50011615766180485
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-09-23 17:00:50 -0300

    Modify client to Hive.

--- lib/models/client.dart
+++ lib/models/client.dart
@@ -1,4 +1,3 @@
-import 'package:app_pesquisa_de_satisfacao/models/model.dart';
 import 'package:hive/hive.dart';
 part 'client.g.dart';
 
@@ -7,33 +6,14 @@ class Client extends HiveObject {
 
   @HiveField(0)
 	String name;
+  
   @HiveField(1)
   String questions;
+
   @HiveField(2)
-  DateTime created;
+  DateTime createdDate;
+
   @HiveField(3)
-	bool complete = false;
-
-	Client(this.name, this.questions, this.created, this.complete);
-
-  Map<String, dynamic> toMap() {
-
-		Map<String, dynamic> map = {
-			'name': name,
-      'questions': questions,
-			'complete': complete
-		};
-
-		return map;
-	}
-
-	static Client fromMap(Map<String, dynamic> map) {
-		return Client(
-			map['id'],
-			map['name'],
-      map['questions'],
-			map['complete'] == 1
-		);
-	}
+	bool completed = false;
 
 }
