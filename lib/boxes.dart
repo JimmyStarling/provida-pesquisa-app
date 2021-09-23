2210c975107eb3c9e068bdd67a408ed180a466eb
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-09-23 16:57:03 -0300

    Add Boxes class.
    Former-commit-id: 107fa35a7014d46d81bab10e2e7610731bbc1441

---
+++ lib/boxes.dart
@@ -0,0 +1,7 @@
+import 'package:app_pesquisa_de_satisfacao/models/client.dart';
+import 'package:hive/hive.dart';
+
+class Boxes {
+  static Box<Client> getClients() =>
+      Hive.box<Client>('client');
+}
