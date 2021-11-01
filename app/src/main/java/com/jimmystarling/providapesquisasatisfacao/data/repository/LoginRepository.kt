2f864361a6981e73c295c50c27388c808d018c25
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-11-01 17:06:55 -0300

    Add new resourcers/files
    Former-commit-id: b8fcce5ce1d2985d76fe18be1f22d88735268df4
    Former-commit-id: f6325a5badacf4440a91f8711ed2c6fbf5b9131a

--- app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/repository/LoginRepository.kt
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/repository/LoginRepository.kt
@@ -1,14 +1,47 @@
-package com.jimmystarling.providapesquisasatisfacao.data
+package com.jimmystarling.providapesquisasatisfacao.data.repository
 
-class LoginRepository(private val dao: LoginDatabaseDao) {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.jimmystarling.providapesquisasatisfacao.data.database.LoginDatabase
+import com.jimmystarling.providapesquisasatisfacao.data.model.LoginTableModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-    val users = dao.getAllUsers()
+class LoginRepository {
 
-    suspend fun insert(user: PesquisadorEntity) {
-        return dao.insert(user)
-    }
+    companion object {
 
-    suspend fun getUserName(userName: String):PesquisadorEntity?{
-        return dao.getUsername(userName)
+        var loginDatabase: LoginDatabase? = null
+
+        var loginTableModel: LiveData<LoginTableModel>? = null
+
+        fun initializeDB(context: Context) : LoginDatabase {
+            return LoginDatabase.getDataseClient(context)
+        }
+
+        fun insertData(context: Context, username: String, password: String) {
+
+            loginDatabase = initializeDB(context)
+
+            CoroutineScope(IO).launch {
+                val loginDetails = LoginTableModel(username, password)
+                loginDatabase!!.loginDao().InsertData(loginDetails)
+            }
+
+        }
+
+        fun getLoginDetails(context: Context, username: String) : LiveData<LoginTableModel>? {
+
+            loginDatabase = initializeDB(context)
+
+            loginTableModel = loginDatabase!!.loginDao().getLoginDetails(username)
+
+            return loginTableModel
+        }
+
     }
 }
--- app/src/main/java/com/jimmystarling/providapesquisasatisfacao/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/ui/login/viewmodel/LoginViewModel.kt
@@ -1,4 +1,23 @@
 package com.jimmystarling.providapesquisasatisfacao.ui.login.viewmodel
 
-class LoginViewModel {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.jimmystarling.providapesquisasatisfacao.data.model.LoginTableModel
+import com.jimmystarling.providapesquisasatisfacao.data.repository.LoginRepository
+
+class LoginViewModel : ViewModel() {
+
+    var liveDataLogin: LiveData<LoginTableModel>? = null
+
+    fun insertData(context: Context, username: String, password: String) {
+        LoginRepository.insertData(context, username, password)
+    }
+
+    fun getLoginDetails(context: Context, username: String) : LiveData<LoginTableModel>? {
+        liveDataLogin = LoginRepository.getLoginDetails(context, username)
+        return liveDataLogin
+    }
+
 }
