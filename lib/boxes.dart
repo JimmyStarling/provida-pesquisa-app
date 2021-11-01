313d8b57269a901af096faa2252763b4de883d8e
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-11-01 11:52:54 -0300

    Change to native.

--- lib/boxes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:app_pesquisa_de_satisfacao/models/client.dart';
-import 'package:hive/hive.dart';
-
-class Boxes {
-  static Box<Client> getClients() =>
-      Hive.box<Client>('clients');
-}
--- lib/models/user.dart
+++
@@ -1,9 +0,0 @@
-class User {
-
-	String name;
-  String questions;
-  DateTime created;
-	bool complete = false;
-
-	User(this.name, this.questions, this.created, this.complete);
-}
