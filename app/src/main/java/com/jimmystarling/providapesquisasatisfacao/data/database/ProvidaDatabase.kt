19ae31a8dd48f6a8608a5cbce0c19c4e295d66df
Jimmy <jimmystarlingdev@gmail.com>
2021-11-08 12:32:26 -0300

    Add new Room Database(Renamed).
    Former-commit-id: ae941216cf8a8d234f5b41271e0b2fc17dd9cec8

---
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/database/ProvidaDatabase.kt
@@ -0,0 +1,38 @@
+package com.jimmystarling.providapesquisasatisfacao.data.database
+
+import android.content.Context
+import androidx.room.*
+import com.jimmystarling.providapesquisasatisfacao.data.model.LoginTableModel
+import com.jimmystarling.providapesquisasatisfacao.data.model.PacienteEntity
+import com.jimmystarling.providapesquisasatisfacao.data.model.PesquisaEntity
+import com.jimmystarling.providapesquisasatisfacao.data.model.PesquisadorEntity
+
+@Database(entities = arrayOf(PesquisadorEntity::class, PacienteEntity::class, PesquisaEntity::class), version = 1, exportSchema = false)
+abstract class ProvidaDatabase : RoomDatabase() {
+
+    abstract fun databaseDao() : DAOAccess
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ProvidaDatabase? = null
+
+        fun getDataseClient(context: Context) : ProvidaDatabase {
+
+            if (INSTANCE != null) return INSTANCE!!
+
+            synchronized(this) {
+
+                INSTANCE = Room
+                    .databaseBuilder(context, ProvidaDatabase::class.java, "PROVIDA_DATABASE")
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                return INSTANCE!!
+
+            }
+        }
+
+    }
+
+}
