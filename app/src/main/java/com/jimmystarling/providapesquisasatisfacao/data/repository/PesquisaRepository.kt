6fd575aadcbb6e7e711d95e995e1c36156a7aa1f
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-11-16 17:16:33 -0300

    Adding changes to pesquisa and pesquisador repository.
    Former-commit-id: 43459cbed1d6733d4e676dfb41958093f33fb066
    Former-commit-id: 3ff5d9b791b4ea28d6aa4365d6eeb9f9a79725d8

--- app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/repository/PesquisaRepository.kt
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/repository/PesquisaRepository.kt
@@ -6,6 +6,7 @@ import com.google.gson.Gson
 import com.jimmystarling.providapesquisasatisfacao.data.database.ProvidaDatabase
 import com.jimmystarling.providapesquisasatisfacao.data.model.PacienteEntity
 import com.jimmystarling.providapesquisasatisfacao.data.model.PesquisaEntity
+import com.jimmystarling.providapesquisasatisfacao.data.model.PesquisadorEntity
 import com.jimmystarling.providapesquisasatisfacao.data.model.QuestaoEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -25,23 +26,24 @@ class PesquisaRepository {
             return ProvidaDatabase.getDataseClient(context)
         }
 
-        fun registerPesquisa(context: Context, questoes: List<QuestaoEntity>, paciente: PacienteEntity) {
+        fun createPesquisa(context: Context, pesquisador: PesquisadorEntity, questoes: List<QuestaoEntity>, paciente: PacienteEntity) {
 
-            LoginRepository.providaDatabase = initializeDB(context)
+            providaDatabase = initializeDB(context)
 
             CoroutineScope(Dispatchers.IO).launch {
-                val pesquisaDetails = PesquisaEntity(gson.toJson(questoes), gson.toJson(paciente))
-                providaDatabase!!.databaseDao().registerPesquisa(pesquisaDetails)
+                val pesquisaDetails = PesquisaEntity(gson.toJson(pesquisador), gson.toJson(questoes), gson.toJson(paciente))
+                providaDatabase!!.databaseDao().createPesquisa(pesquisaDetails)
             }
 
         }
 
-        fun updatePesquisa(context: Context, pesquisaEntity: PesquisaEntity, questoes: List<QuestaoEntity>, paciente: PacienteEntity) {
+        // Here will pass the appended variable to questoes list and convert it to json
+        fun updatePesquisa(context: Context, pesquisa: PesquisaEntity, pesquisador: PesquisadorEntity, questoes: List<QuestaoEntity>, paciente: PacienteEntity) {
 
-            LoginRepository.providaDatabase = initializeDB(context)
+            providaDatabase = initializeDB(context)
 
             CoroutineScope(Dispatchers.IO).launch {
-                providaDatabase!!.databaseDao().updatePesquisa(pesquisaEntity.Id, gson.toJson(questoes))
+                providaDatabase!!.databaseDao().updatePesquisa(pesquisa.Id, gson.toJson(pesquisador), gson.toJson(questoes), gson.toJson(paciente))
             }
 
         }
@@ -54,6 +56,18 @@ class PesquisaRepository {
 
             return pesquisaEntity
 
+        }
+
+        // Here will pass the variable pesquisa appended to pesquisas converted to json to updatePesquisador
+        fun updatePesquisador(context: Context, pesquisador: PesquisadorEntity, pesquisas: List<PesquisaEntity>, quantidade_pesquisa: Int){
+            providaDatabase = initializeDB(context)
+
+            val pesquisas_json = gson.toJson(pesquisas)
+            val quantidade_pesquisas_json = gson.toJson(quantidade_pesquisa)
+
+            CoroutineScope(Dispatchers.IO).launch {
+                providaDatabase!!.databaseDao().updatePesquisador(pesquisador.Id, pesquisas_json, quantidade_pesquisas_json)
+            }
         }
     }
 }
