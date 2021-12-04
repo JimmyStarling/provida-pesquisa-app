1c2fbd4e5109c9572733190faa6956884315c3b8
Jimmy <jimmystarlingdev@gmail.com>
2021-12-04 16:21:49 -0300

    Serialize.
    Former-commit-id: fded693b4e564420d9bb6fa8d5dfaf9b005b1b20

--- app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/model/QuestaoEntity.kt
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/model/QuestaoEntity.kt
@@ -1,15 +1,20 @@
 package com.jimmystarling.providapesquisasatisfacao.data.model
 
-import androidx.room.ColumnInfo
-import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class QuestaoEntity(
+    @SerializedName("id")
     var id: Int? = null,
     //@ColumnInfo(name = "title")
+    @SerializedName("title")
     var title: String,
     //@ColumnInfo(name = "content")
+    @SerializedName("content")
     var content: String,
     //@ColumnInfo(name = "resposta")
+    @SerializedName("resposta")
     var resposta: String,
 )/*{
     @PrimaryKey(autoGenerate = true)
