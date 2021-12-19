d6fa3f94a54c384af937b9c38ad2fd0c28f3c1a4
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-12-19 09:52:01 -0300

    Creating global inline function to be used to convert the questions json from pesquisa.
    Former-commit-id: 7a04a7c6e69b97226c6dc8165d478e2a111e37c0
    Former-commit-id: 8fde7f2dd504779a7d41d5428ad504d478b11509

--- app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/converter/Converter.kt
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/data/converter/Converter.kt
@@ -1,6 +1,9 @@
 package com.jimmystarling.providapesquisasatisfacao.data.converter
 
 import androidx.room.TypeConverter
+import com.google.common.reflect.TypeToken
+import com.google.gson.Gson
 
 class Converter {
 }
+inline fun <reified T> Gson.fromJson(json: String) = fromJson<T>(json, object: TypeToken<T>() {}.type)
