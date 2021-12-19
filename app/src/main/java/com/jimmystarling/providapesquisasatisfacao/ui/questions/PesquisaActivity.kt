42c6dc2849e35572de879733fca3a65cd750d535
Jimmy Starling <jimmystarlingdev@gmail.com>
2021-12-19 16:54:21 -0300

    Rename.
    Former-commit-id: cc5b75d6e14eea959dbafb3925735e5a74260fe4
    Former-commit-id: 19625b9f2945ef262cc3a9cfe032e61ce6cec280

--- app/src/main/java/com/jimmystarling/providapesquisasatisfacao/ui/questions/PesquisaActivity.kt
+++ app/src/main/java/com/jimmystarling/providapesquisasatisfacao/ui/questions/PesquisaActivity.kt
@@ -1,15 +1,37 @@
 package com.jimmystarling.providapesquisasatisfacao.ui.questions
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.slider.Slider
 import com.jimmystarling.providapesquisasatisfacao.R
+import com.jimmystarling.providapesquisasatisfacao.data.model.PacienteEntity
+import com.jimmystarling.providapesquisasatisfacao.data.model.PesquisaEntity
+import com.jimmystarling.providapesquisasatisfacao.data.model.PesquisadorEntity
 import com.jimmystarling.providapesquisasatisfacao.data.model.QuestaoEntity
 import java.text.SimpleDateFormat
 import java.util.*
 
 class PesquisaActivity : AppCompatActivity() {
+
+    // From activity
+    val appContext = this.application!!.applicationContext
+    // Fragments
+    lateinit var lastFragment: Fragment
+    lateinit var nextFragment: Fragment
+    // Entities and variables to modelview
+    lateinit var mResearcher: PesquisadorEntity
+    lateinit var mPatient: PacienteEntity
+    lateinit var mQuestions: MutableList<QuestaoEntity>
+    lateinit var mPesquisa: PesquisaEntity
+    // Variables
+    lateinit var slider: Slider
+    lateinit var sliderValue: String
+    lateinit var teamTitles: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +43,14 @@ class PesquisaActivity : AppCompatActivity() {
                 .commitNow()
         }
     }
-
     companion object{
         var QUESTOES: String? = null
-        val simpleFormat = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
-        val currentDate = simpleFormat.format(Date())
-
-        lateinit var mTitleQuestion : TextView
-        lateinit var mTitleContent: TextView
-
         lateinit var activityPesquisa: Any
-
+        fun getCurrentDate(): String {
+            @SuppressLint("SimpleDateFormat")
+            val simpleFormat = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentDate = simpleFormat.format(Date())
+            return currentDate
+        }
     }
 }
